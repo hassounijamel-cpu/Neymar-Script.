d94016c66e936272b5e52d6fe06b895b884e3b53
hassounijamel-cpu <hassounijamel@gmail.com>
2025-09-04 17:26:34 +0300

    Update Neymar.lua
    Lopsmid

--- Neymar.lua
+++ Neymar.lua
@@ -10,7 +10,7 @@ local Window = Libary:MakeWindow({
     Flags = "NeymarHub_Broookhaven"
 })
 Window:AddMinimizeButton({
-    Button = { Image = "rbxassetid://131669852271916", BackgroundTransparency = 0 },
+    Button = { Image = "rbxassetid://81886512783938", BackgroundTransparency = 0 },
     Corner = { CornerRadius = UDim.new(35, 1) },
 })
 
